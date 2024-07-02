a48dee5870d74e0271d7dd16a480334ac5287e82
Jake Rubach <jake.d.rubach@gmail.com>
2024-07-02 14:02:48 -0400

    Made sure to add min and max to all # values for validation later. Added Difficulty Setting tooltips, and added encounter difficulty page with tooltips. Remove Bin button appears correctly and functions correctly, but will not reappear after it disappears once.

---
+++ js/encounterDifficulty.js
@@ -0,0 +1,26 @@
+const encounterDifficultyTab = document.getElementById('encounter-difficulty-tab');
+
+const removeEncounterDifficultyButton = document.getElementById('remove-encounter-difficulty-bin');
+
+document.addEventListener('click', function(e) {
+    var addEncounterDifficultyButton = e.target.closest('#add-encounter-difficulty-bin');
+    var removeEncounterDifficulty = e.target.closest('#remove-encounter-difficulty-bin');
+    if (addEncounterDifficultyButton) {
+        if (removeEncounterDifficultyButton.classList.contains('hide')) {
+            removeEncounterDifficultyButton.classList.remove('hide');
+            removeEncounterDifficultyButton.classList.add('show');
+        }
+        var encounterBins = document.querySelectorAll('.encounter-difficulty-bin-row');
+        var encounterBinRowNumber = encounterBins.length + 1;
+        encounterDifficultyTab.innerHTML += '<div class="row encounter-difficulty-bin-row"><div class="col-4"><mwc-textfield type="number" step=".01" min="0" max="1" class="w-100 mt-1" id="encounter-difficulty-weight-bin-'+encounterBinRowNumber+'" label="Weight" maxlength="4" charcounter></mwc-textfield></div><div class="col-4"><mwc-textfield type="number" min="0" max="9999" class="w-100 mt-1" id="encounter-difficulty-min-bin-'+encounterBinRowNumber+'" label="Min" maxlength="4" charcounter></mwc-textfield></div><div class="col-4"><mwc-textfield type="number" min="0" max="9999" class="w-100 mt-1" id="encounter-difficulty-max-bin-'+encounterBinRowNumber+'" label="Max" maxlength="4" charcounter></mwc-textfield></div></div>'
+    } else if (removeEncounterDifficulty) {
+        var encounterBins = document.querySelectorAll('.encounter-difficulty-bin-row');
+        var lastEncounterBin = encounterBins[encounterBins.length-1];
+        lastEncounterBin.parentNode.removeChild(lastEncounterBin);
+        var updatedEncounterBins = document.querySelectorAll('.encounter-difficulty-bin-row');
+        if (updatedEncounterBins.length === 1) {
+            removeEncounterDifficulty.classList.remove('show');
+            removeEncounterDifficulty.classList.add('hide');
+        }
+    }
+});
